53d09ae28654dea021d1c22a3f9d26094a38c7f0
Juan José Nolasco <nolascomeza18@gmail.com>
2018-01-22 22:35:47 -0700

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <p>Completed Task: {{ todos.filter(todo => {return todo.done == true}).length}}</p>
+    <p>Pending Task: {{todos.filter(todo => {return todo.done == false}).lenght}}</p>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo" ></todo>
+</div>
+</template>0
+<script>
+import Todo from './Todo';
+export default {
+  props: ['todos'],
+  components: {
+      Todo,
+  },
+  methods: {
+      deleteTodo(todo) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos.splice(todoIndex, 1);
+      },
+      completeTodo(todo) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos[todoIndex].done = true;
+      },
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
