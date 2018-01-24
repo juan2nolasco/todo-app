2a3eab6ef9fa9d3e460f605aa5fac8fb7d67db2d
Juan José Nolasco <nolascomeza18@gmail.com>
2018-01-23 22:46:10 -0700

    add sweet alerts for creating

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-//import sweetalert from 'sweetalert';
+import sweetalert from 'sweetalert';
 import TodoList from './components/TodoList';
 import CreateTodo from './components/CreateTodo';
 
@@ -45,7 +45,7 @@ export default {
   methods: {
     createTodo(newTodo) {
       this.todos.push(newTodo);
-      //sweetalert('Success!','To-Do created!', 'success');
+      sweetalert('Success!','To-Do created!', 'success');
     },
   },
 };
